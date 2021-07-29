67d436433fd32e5ec3fd0f5c50f44ed59bfe3b4e
andreysc1976 <andreysc@yandex.ru>
2021-07-29 15:40:42 +0300

    готово

---
+++ app/src/main/java/ru/a_party/hw6_notes/AboutFragment.java
@@ -0,0 +1,39 @@
+package ru.a_party.hw6_notes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AboutFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AboutFragment extends Fragment {
+
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static AboutFragment newInstance() {
+        return new AboutFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
--- app/src/main/java/ru/a_party/hw6_notes/MainActivity.java
+++ app/src/main/java/ru/a_party/hw6_notes/MainActivity.java
@@ -1,23 +1,70 @@
 package ru.a_party.hw6_notes;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Menu;
+
 import android.view.MenuItem;
-import android.widget.CompoundButton;
-import android.widget.RadioButton;
+import android.view.View;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState==null) Note.generateDemoNotes();
-        setContentView(R.layout.activity_main);
-        setSupportActionBar(findViewById(R.id.toolbar));
+        setContentView(R.layout.activity_main_m);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        navigationView = findViewById(R.id.navView);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.openStr, R.string.closeStr);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        toggle.setDrawerIndicatorEnabled(false);
+
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_baseline_dehaze_24, this.getTheme());
+        toggle.setHomeAsUpIndicator(drawable);
+        toggle.setToolbarNavigationClickListener(v -> {
+            if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId()==R.id.itemAbout){
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack(null)
+                        .replace(R.id.fragmentContainerNoteList,AboutFragment.newInstance())
+                        .commit();
+            } else if (item.getItemId()==R.id.itemExit){
+                finish();
+            }
+            return false;
+        });
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragmentContainerNoteList,NoteListFragment.newInstance())
@@ -25,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 
 
 }
